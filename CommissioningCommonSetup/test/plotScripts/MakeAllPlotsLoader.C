3a035332e21000269b3eff14749f9409f862967b
jyothsna <>
2010-07-02 12:41:05 +0000

    Add TrackSelector in the compilation macro.

--- CommissioningCommonSetup/test/plotScripts/MakeAllPlotsLoader.C
+++ CommissioningCommonSetup/test/plotScripts/MakeAllPlotsLoader.C
@@ -1,5 +1,6 @@
 void MakeAllPlotsLoader(string mcfilename, string datafilename, string plotfilename, double finalNorm=-1, string rootFile="plots.root" , string doPlot="")
 {
+  if(gSystem->Load("TrackSelector_C.so")<0)gSystem->CompileMacro("TrackSelector.C","Ok");
   if(gSystem->Load("TSelectorMultiDraw_C.so")<0)gSystem->CompileMacro("TSelectorMultiDraw.C","Ok");
   if(gSystem->Load("MakeAllPlots_C.so")<0)gSystem->CompileMacro("MakeAllPlots.C","Ok");
   MakeAllPlots(mcfilename,datafilename,plotfilename,finalNorm, rootFile, doPlot);
